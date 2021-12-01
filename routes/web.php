115eadd300c1869b147f1c68ac28c87b4029705f
Theodorus Revonsio Prananta <revonsio12@gmail.com>
2021-12-01 18:40:05 +0700

    Menambahkan CRUD tabel absen

--- routes/web.php
+++ routes/web.php
@@ -42,3 +42,10 @@ Route::get('/pendapatan/edit/{id}','PendapatanController@edit');
 Route::post('/pendapatan/update','PendapatanController@update');
 Route::get('/pendapatan/hapus/{id}','PendapatanController@hapus');
 
+//route CRUD tabel absen
+Route::get('/absen','AbsenController@index');
+Route::get('/absen/tambah','AbsenController@tambah');
+Route::post('/absen/store','AbsenController@store');
+Route::get('/absen/edit/{id}','AbsenController@edit');
+Route::post('/absen/update','AbsenController@update');
+Route::get('/absen/hapus/{id}','AbsenController@hapus');
